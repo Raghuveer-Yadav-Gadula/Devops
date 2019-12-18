8bb2b583a9cfa9d7946007b1bc8015508dcd85e0
Raghuveer Yadav (Data Glove IT Solutions Pvt. L) <v-ragya@microsoft.com>
2019-12-18 18:36:12 +0530

    updated script with vault name and policy name

--- az-recoveryvault-vmbackup-retention-cli.sh
+++ az-recoveryvault-vmbackup-retention-cli.sh
@@ -5,13 +5,18 @@ RESOURCEGROUP=$1
 RECOVERYSERVICEVAULT=$2
 VM_NAME=$3
 DATE=$4
+AZBKPPOLICYNAME=$5
+
 
 
 # Create recovery vault
-az backup vault create --name $RGROUP --name $RVAULT --location $LOCATION
+az backup vault create --name $RESOURCEGROUP --name $RECOVERYSERVICEVAULT --location $LOCATION
 
 #Create/enable default backup policy for a VM(the default policy is for 30days)
-az backup protection-for-vm --resource-group $RGROUP --vault-name $RVAULT --vm $VM_NAME --policy-name DefaultPolicy
+#az backup protection-for-vm --resource-group $RESOURCEGROUP --vault-name $RECOVERYSERVICEVAULT --vm $VM_NAME --policy-name DefaultPolicy
 
 #Custom policy with the backup retention date format in UTC format dd-mm-yyyy
-az backup protection backup-now --resource-group $RGROUP --vault-name $RVAULT --vm $VM_NAME --retention $DATE
+#az backup protection backup-now --resource-group $RGROUP --vault-name $RVAULT --vm $VM_NAME --retention $DATE
+
+#create new backup policy with BackupManagementType and workload Type
+az backup policy create --name {azbackup} --resource-group $RESOURCEGROUP --vault-name $RECOVERYSERVICEVAULT --name $AZBKPPOLICYNAME
