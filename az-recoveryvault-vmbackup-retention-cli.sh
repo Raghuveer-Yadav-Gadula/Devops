535f8b517ba528ff8513eaf8aa561e703b9f697a
Raghuveer Yadav (Data Glove IT Solutions Pvt. L) <v-ragya@microsoft.com>
2019-12-20 13:44:05 +0530

    updated one

--- az-recoveryvault-vmbackup-retention-cli.sh
+++ az-recoveryvault-vmbackup-retention-cli.sh
@@ -19,7 +19,9 @@ az backup vault create --name $RESOURCEGROUP --name $RECOVERYSERVICEVAULT --loca
 #az backup protection backup-now --resource-group $RGROUP --vault-name $RVAULT --vm $VM_NAME --retention $DATE
 
 #usage of az show and parsing in json template
-az vm show -g policy{policy}
+az vm show -g policy{az-backup-vault-creation.json}
+
+#source of the json template "https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-manager-keyvault-parameter"
 
 #create new backup policy with BackupManagementType and workload Type
 az backup policy create --name {azbackup} --resource-group $RESOURCEGROUP --vault-name $RECOVERYSERVICEVAULT --name $AZBKPPOLICYNAME [--subscription $SUBSCRIPTION]
